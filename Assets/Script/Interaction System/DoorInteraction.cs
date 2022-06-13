328e60ce567e11a4262d660147161f52574b2676
Kaploy <sauloedu@yahoo.com>
2022-06-13 16:06:01 -0400

    Animação da porta

---
+++ Assets/Script/Interaction System/DoorInteraction.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteraction : Interactable
+{
+    // Start is called before the first frame update
+    public Animator doorAnim;
+    public bool doorOpen = false;
+
+    private void Awake()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();
+    }
+    public override string GetDescription()
+    {
+        if (!doorOpen)
+        {
+            return "Door(Open)";
+        }
+        else
+        {
+            return "Door(Close)";
+        }
+
+    }
+
+
+    public override void Interact()
+    {
+        if (!doorOpen)
+        {
+            doorAnim.Play("door_opening");
+            doorOpen = true;
+        }
+        else
+        {
+            
+            doorAnim.Play("door_closing");
+            doorOpen = false;
+        }
+       
+
+    }
+}
